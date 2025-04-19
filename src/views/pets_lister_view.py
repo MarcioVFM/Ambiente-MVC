087e3e2f1f3f18f7efea9ed3c035a24a374efacc
Marcio Vinicius <marciov859@gmail.com>
2025-04-19 10:23:14 -0400

    feat: implementinng project routes for pets and people

--- src/views/pets_lister_view.py
+++ src/views/pets_lister_view.py
@@ -3,7 +3,7 @@ from .http_types.http_request import HttpRequest
 from .http_types.http_response import HttpResponse
 from .interface.view_interface import ViewInterface
 
-class PetListerController(ViewInterface):
+class PetListerView(ViewInterface):
     def __init__(self, controller: PetListerControllerInterface) -> None:
         self.__controller = controller
 
